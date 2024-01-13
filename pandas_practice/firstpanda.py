2d61499b2a34365456d5bd8f23a03fc60fdb55e7
PoohL0ve <screwed06@gmail.com>
2024-01-13 16:06:29 -0400

    Using iloc and loc to obtain similar values

--- pandas_practice/firstpanda.py
+++ pandas_practice/firstpanda.py
@@ -2,14 +2,16 @@ import pandas as pd
 
 events = [8, 13, 18, 22, 31]
 my_column = pd.Series(events)
-print(my_column)
+#print(my_column)
 
 careers = {'Careers' : ['Data Analyst', 'Backend Developer', 'Computer Programmer', 'App Developer'],
            'Language': ['SQL', 'Python', 'Assembly', 'Java'],
            'Resources': ['YouTube', 'Coursera', 'Books', 'HyperSkill']}
 
-frame_careers = pd.DataFrame(careers)
-print(frame_careers)
+frame_careers = pd.DataFrame(careers, index= ['Fun', 'Professional', 'Testing', 'Passion'])
+#print(frame_careers)
 
 guess = frame_careers.iloc[1]
+check = frame_careers.loc['Professional']
 print(guess)
+print(check)
